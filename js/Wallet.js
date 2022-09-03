c6e606f55fcc25d0988a5ca22be76dac6ca31cbf
romoguill <romoguill@gmail.com>
2022-09-03 20:00:00 -0300

    Dumped some test data from Binance API to work with

--- js/Wallet.js
+++ js/Wallet.js
@@ -6,6 +6,12 @@ class Wallet {
   addCryptocurrency(cryptocurrency, quantity) {
     this.cryptocurrencies.push({ cryptocurrency, quantity });
   }
+
+  removeCryptocurrency(token) {
+    this.cryptocurrencies = this.cryptocurrencies.filter(
+      (cryptocurrency) => cryptocurrency.token === token
+    );
+  }
 }
 
 export default Wallet;
