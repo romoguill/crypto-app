15f61d53b99bc22869aa775036de0a6dd093aa7b
romoguill <romoguill@gmail.com>
2022-09-14 12:05:29 -0300

    Added a function that renders dynamically the contents of the Wallet

--- js/simulator.js
+++ js/simulator.js
@@ -8,3 +8,27 @@ const wallet = new Wallet();
 
 const bitcoin = new Cryptocurrency('BTC', 'Bitcoin', bitcoinHistory);
 const ethereum = new Cryptocurrency('ETH', 'Ethereum', ethereumHistory);
+
+const walletListElement = document.getElementById('wallet-items-list');
+
+wallet.addCryptocurrency(bitcoin, 3.43);
+wallet.addCryptocurrency(ethereum, 45.12);
+
+function renderWallet() {
+  let fragment = document.createDocumentFragment();
+
+  wallet.cryptocurrencies.forEach((walletItem) => {
+    const liElement = document.createElement('li');
+    liElement.classList.add('list-group-item');
+    liElement.innerHTML = `
+        <div class="d-flex justify-content-between">
+          <p>${walletItem.cryptocurrency.name}</p>
+          <p>${walletItem.quantity}</p>
+        </div>
+    `;
+    fragment.appendChild(liElement);
+  });
+  walletListElement.append(fragment);
+}
+
+renderWallet();
