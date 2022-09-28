1a31d7784e5defca2a6c26a1796990ef0cf8ab0e
romoguill <romoguill@gmail.com>
2022-09-28 08:15:34 -0300

    Refactored the handle functions to its own functions

--- js/simulator.js
+++ js/simulator.js
@@ -5,11 +5,14 @@ let wallet = new Wallet();
 
 const bitcoin = new Cryptocurrency('BTC', 'Bitcoin');
 const ethereum = new Cryptocurrency('ETH', 'Ethereum');
+const binance = new Cryptocurrency('BNB', 'Binance');
+const xrp = new Cryptocurrency('XRP', 'XRP');
 
 const walletListElement = document.getElementById('wallet-items-list');
 const editWalletFormElement = document.getElementById('edit-wallet-form');
 const addItemButtonElement = document.getElementById('add-item');
 const saveFormButtonElement = document.getElementById('save-form');
+const openFormButtonElement = document.getElementById('open-modal-button');
 
 wallet.addCryptocurrency(bitcoin, 3.43);
 wallet.addCryptocurrency(ethereum, 45.12);
@@ -21,9 +24,9 @@ function renderWallet() {
     const liElement = document.createElement('li');
     liElement.classList.add('list-group-item');
     liElement.innerHTML = `
-        <div class="d-flex justify-content-between">
-          <p>${walletItem.cryptocurrency.name}</p>
-          <p>${walletItem.quantity}</p>
+        <div class="d-flex justify-content-between align-items-center my-2">
+          <span>${walletItem.cryptocurrency.name}</span>
+          <span>${walletItem.quantity}</span>
         </div>
     `;
     fragment.appendChild(liElement);
@@ -74,6 +77,12 @@ function renderForm() {
 function handleSave() {
   let formData = new FormData(document.getElementById('edit-wallet-form'));
 
+  // First check if there was an item deleted
+  wallet.cryptocurrencies = wallet.cryptocurrencies.filter((walletItem) =>
+    formData.has(walletItem.cryptocurrency.name)
+  );
+
+  // Then check if there was a change in the quantity or if its a new crypto
   for (let [name, quantity] of formData.entries()) {
     if (wallet.hasCryptocurrency(name)) {
       wallet.modifyQuantity(name, parseFloat(quantity));
@@ -88,47 +97,52 @@ function handleSave() {
   renderWallet();
 }
 
-function handleDelete() {
-  // TODO
+function handleDelete(e) {
+  if (e.target.classList.contains('btn-delete-item')) {
+    const walletItemElement = e.target.closest('[data-cryptocurrency]');
+    walletItemElement.remove();
+  }
 }
 
-addItemButtonElement.addEventListener('click', () => {
-  console.warn('TODO: todavia no esta implementadas otras cryptos');
+function handleAddItem() {
   const walletItemContainerElement = document.createElement('div');
-  walletItemContainerElement.className =
-    'container d-flex justify-content-between align-items-center mb-2';
+  walletItemContainerElement.className = 'container mb-2';
+  // walletItemContainerElement.setAttribute(
+  //   'data-cryptocurrency',
+  //   walletItem.cryptocurrency.name
+  // );
   walletItemContainerElement.innerHTML = `
-    <input
-      type="text"
-      class="form-control cryptocurrency-input"
-      placeholder="Cryptocurrency"
-    />
-    <input
-      type="number"
-      class="form-control quantity-input"
-      placeholder="Quantity"
-    />
-  `;
+      <div class="row align-items-center">
+        <div class="col-6">
+          <input 
+            type="text" 
+            class="text-start form-control quantity-input" 
+            placeholder="Cryptocurrency"
+          >
+        </div>
+        <div class="col-5">
+          <input
+            type="number"
+            class="text-end form-control quantity-input"
+            placeholder="Quantity"
+          />
+        </div>
+        <div class="col-1">
+          <i class="bi bi-x text-danger btn-delete-item" role="button"></i>
+        </div>
+      </div>
+      `;
 
   editWalletFormElement.appendChild(walletItemContainerElement);
-});
+}
+
+openFormButtonElement.addEventListener('click', renderForm);
 
 saveFormButtonElement.addEventListener('click', handleSave);
 
+addItemButtonElement.addEventListener('click', handleAddItem);
+
+editWalletFormElement.addEventListener('click', handleDelete);
+
 renderWallet();
 renderForm();
-
-// const walletStr = localStorage.getItem('wallet');
-// console.log(walletStr);
-// wallet = Wallet.fromJSON(walletStr);
-// console.log(wallet);
-
-editWalletFormElement.addEventListener('click', (e) => {
-  if (e.target.classList.contains('btn-delete-item')) {
-    const walletItemElement = e.target.closest('[data-cryptocurrency]');
-    const cryptocurrency = walletItemElement.dataset.cryptocurrency;
-    wallet.removeCryptocurrency(cryptocurrency);
-    console.log(wallet);
-    renderForm();
-  }
-});
