a59358ad706cefb7934d7cf298ffe30b427c9e41
romoguill <romoguill@gmail.com>
2022-10-28 20:34:22 -0300

    Finished the methods to calculate the total daily value of the content of the wallet

--- js/chartUtil.js
+++ js/chartUtil.js
@@ -11,27 +11,82 @@ const wallet = localStorage.getItem('wallet')
   ? Wallet.fromJSON(localStorage.getItem('wallet'))
   : new Wallet();
 
-wallet.cryptocurrencies.map(async (walletItem) => {
-  const data = await getCryptocurrencyHistory(walletItem.cryptocurrency.token);
-  console.log(data);
-});
+function getUKLines() {
+  return Promise.all(
+    wallet.cryptocurrencies.map(async (walletItem) => {
+      const data = await getCryptocurrencyHistory(
+        walletItem.cryptocurrency.token
+      );
+      const dateTimes = data.map((dayTicker) => dayTicker[0]);
+      const closingPrices = data.map((dayTicker) => dayTicker[4]);
+      return [dateTimes, closingPrices];
+    })
+  );
+}
 
-const data = {
-  labels: bitcoin.getDataForChart().labels,
-  datasets: [
-    {
-      label: 'Wallet',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: bitcoin.getDataForChart().dataClosePrice,
+async function calculateWalletHistory() {
+  const historyData = await getUKLines();
+
+  // Date time will be the same for each crypto, so I'll take the first one
+  const dateTimes = historyData[0][0];
+
+  const totalValuesHistory = [];
+  for (let i = 0; i < historyData.length; i++) {
+    totalValuesHistory.push(
+      historyData[i][1].map((cryptoValue) => {
+        return cryptoValue * wallet.cryptocurrencies[i].quantity;
+      })
+    );
+  }
+
+  console.log(totalValuesHistory);
+
+  const walletHistoryTotals = [];
+  for (let i = 0; i < totalValuesHistory[0].length; i++) {
+    let dailyTotal = 0;
+    for (let j = 0; j < totalValuesHistory.length; j++) {
+      dailyTotal += totalValuesHistory[j][i];
+    }
+    walletHistoryTotals.push(dailyTotal);
+  }
+  return walletHistoryTotals;
+}
+
+async function renderChart() {
+  const historyData = await getUKLines();
+
+  // Date time will be the same for each crypto, so I'll take the first one
+  let dateTimes = historyData[0][0];
+
+  dateTimes = dateTimes.map((date) => new Date(date).toDateString());
+
+  const walletHistoryTotals = await calculateWalletHistory();
+
+  console.log(walletHistoryTotals);
+  const data = {
+    labels: dateTimes,
+    datasets: [
+      {
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: walletHistoryTotals,
+      },
+    ],
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
     },
-  ],
-};
+  };
 
-const config = {
-  type: 'line',
-  data: data,
-  options: {},
-};
+  const myChart = new Chart(document.getElementById('testChart'), config);
+}
 
-const myChart = new Chart(document.getElementById('testChart'), config);
+renderChart();
